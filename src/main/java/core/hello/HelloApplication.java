e82ddcaa4d00a08ca5e2a9741cf9a8b74c019d14
kim heesik <hesedhs@gmail.com>
2023-01-11 23:20:48 +0900

    core v1

--- src/main/java/core/hello/HelloApplication.java
+++ src/main/java/core/hello/HelloApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloApplication {
-
+// test
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 
